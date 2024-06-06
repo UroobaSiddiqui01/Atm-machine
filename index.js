694ccab948b8c39bc04f6ca8b20cf955be354c40
UroobaSiddiqui01 <160516454+UroobaSiddiqui01@users.noreply.github.com>
2024-06-06 08:07:55 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,52 @@
+import inquirer from "inquirer";
+let myBalance = 10000;
+let myPin = 1234;
+let pinAnswer = await inquirer.prompt([
+    {
+        name: "pin",
+        message: "enter youre pin",
+        type: "number"
+    }
+]);
+if (pinAnswer.pin === myPin) {
+    console.log("correct pin code!!!");
+    let operationAns = await inquirer.prompt([{
+            name: "operation",
+            message: "piesae select option",
+            type: "list",
+            choices: ["withdraw", "check balance",]
+        }]);
+    console.log(operationAns);
+    if (operationAns.operation === 'withdraw') {
+        let amountAns = await inquirer.prompt([{
+                name: "amount",
+                message: "enter your amount",
+                type: "number"
+            }]);
+        if (amountAns.account <= myBalance) {
+            myBalance -= amountAns.account;
+            console.log(`your remaining balance is : ${myBalance}`);
+        }
+        else {
+            console.log("insufficient balance!");
+        }
+        if (operationAns.operation === "check Balance") {
+            console.log(`ypur balance is ${myBalance}`);
+        }
+    }
+    else if (operationAns.operation === "Fast cash") {
+        let fastCashAns = await inquirer.prompt([
+            {
+                name: "fastcash",
+                message: "How much amount your want to fast cash :",
+                type: "list",
+                choices: [1000, 2000, 5000, 10000]
+            }
+        ]);
+        myBalance -= fastCashAns.fastCash;
+        console.log(`your remaining amount is ${myBalance}`);
+    }
+}
+else {
+    console.log("incorrect pin code");
+}
